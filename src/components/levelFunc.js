168baafa02b5a283f26b4a00bf563576dc4f6174
ausraf720 <14104@coderacademy.edu.au>
2023-09-08 18:10:37 +1000

    finished comments completely

--- src/components/levelFunc.js
+++ src/components/levelFunc.js
@@ -1,10 +1,22 @@
 
+/* this function is for converting front-end level to back-end level object and vice versa,
+object is object which level data is obtained from, topics is list of topics,
+reverse is a bool indicated if transferring from front-end to back-end or other way around
+*/
 export default function levelFunc(object, topics, reverse) {
+    
+    // levelObject store values of level for each topic to be returned
     const levelObject = {}
+
+    // if getting data from back-end to set front-end data, reverse not true
     if (!reverse) {
+
+        // iterate through all topics to set values for levelObject to be returned
         for (const key of topics) {
             levelObject[key] = object[`${key.toLowerCase()}Level`]
         }
+
+    // do the same but reverse key names, so that front-end data can be sent back to back-end
     } else if (reverse) {
         for (const key of topics) {
             levelObject[`${key.toLowerCase()}Level`] = object[key]
--- src/components/questionHandler.js
+++ src/components/questionHandler.js
@@ -1,14 +1,26 @@
+/* this function alerts user if answer is right/wrong,
+also updates level for topic is user is right,
+q is actual question object, selection is chosen option, 
+levels is user's levels, name is current topic, maxLevel is max possible level for topic
+*/
 export default function questionHandler(q, selection, levels, name, maxLevel) {
+
+    // if selection matches answer, alert user to be correct
     if (selection == q.answer) {
         alert("Answer was correct!")
+
+        // don't increment level if maxLevel already reached
         if (levels[name] == maxLevel) {
             alert("Max level reached!")
             return levels
+
+        // otherwise increment level for that topic
         } else {
             levels[name] += 1
             return levels
         }
-        
+    
+    // notify user if selection is wrong and don't level-up
     } else {
         alert("Answer was wrong!")
         return levels
